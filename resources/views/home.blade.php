acdcf87a859bd157f1f66984024d87f70328fa6f
Jeferson Fernandes <dev.jefersonl@gmail.com>
2023-12-07 15:42:34 -0300

    correcao na visibilidade dos itens da navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,17 +15,38 @@
             <div class="collapse navbar-collapse justify-content-center" id="navbarCenteredExample">
                 <!-- Left links -->
                 <ul class="navbar-nav mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">Portal</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">Atividades</a>
-                    </li>
                     @if ($user)
-                        @if ($userRole->role_id > 2)
+                        @if ($userRole->role_id == 1)
+                            <li class="nav-item">
+                                <a class="nav-link active" href=""></a>
+                            </li>
+                        @elseif ($userRole->role_id == 2)
+                            <li class="nav-item">
+                                <a class="nav-link active" href="">Portal</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="">Atividades</a>
+                            </li>
+                        @elseif ($userRole->role_id == 3)
+                            <li class="nav-item">
+                                <a class="nav-link active" href="">Portal</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="">Atividades</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ route('student.index') }}">Alunos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ route('diciplines.index') }}">Diciplinas</a>
+                            </li>
+                        @elseif ($userRole->role_id == 4)
+                            <li class="nav-item">
+                                <a class="nav-link active" href="">Portal</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="">Atividades</a>
+                            </li>
                             <li class="nav-item">
                                 <a class="nav-link active" href="{{ route('student.index') }}">Alunos</a>
                             </li>
@@ -35,18 +56,8 @@
                             <li class="nav-item">
                                 <a class="nav-link active" href="{{ route('diciplines.index') }}">Diciplinas</a>
                             </li>
-                        @else
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#">Teste</a>
-                            </li>
                         @endif
                     @endif
-                    <li class="nav-item">
-                        <a class="nav-link disabled"></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled"></a>
-                    </li>
                     @if (!$user)
                         <li class="nav-item">
                             <a class="nav-link active" href="{{ route('authlogin.index') }}">Login</a>
@@ -69,7 +80,6 @@
                             </ul>
                         </li>
                     @endif
-
                 </ul>
                 <!-- Left links -->
             </div>
