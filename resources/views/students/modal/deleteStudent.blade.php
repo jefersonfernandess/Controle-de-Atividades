5e7f9fcc3e2b3693b5aa5e65525df33a0383be7c
Jeferson Fernandes <dev.jefersonl@gmail.com>
2023-12-12 15:39:54 -0300

    atualização nas regras e views dos alunos

--- resources/views/students/modal/deleteStudent.blade.php
+++ resources/views/students/modal/deleteStudent.blade.php
@@ -4,24 +4,22 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <p class="modal-title fs-5" id="staticBackdropLabel">Você quer *<b>APAGAR</b>* ou *<b>DESVINCULAR</b>* a conta desse aluno(a)?</p>
+                <p class="modal-title fs-5" id="staticBackdropLabel">Você quer *<b>DESVINCULAR</b>* esse aluno(a)?</p>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body d-flex justify-content-center gap-2">
-                <form action="{{ route('student.unlink', $user->id) }}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-warning">Desvincular</button>
-                </form>
-                <form action="{{ route('student.destroy', $user->id) }}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Apagar</button>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Voltar</button>
+                <div>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Voltar</button>
+                </div>
+                <div>
+                    <form action="{{ route('student.unlink', $user->id) }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger" data-bs-dismiss="modal">Desvincular</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
+</div>
 </div>
